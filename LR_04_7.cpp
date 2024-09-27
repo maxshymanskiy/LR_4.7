b759360b6efa44511637affa13cff1593d074167
max-shymanskiy <maxshymanskiy@gmail.com>
2024-09-27 20:16:35 +0300

    corect-code

--- LR_04_7.cpp
+++ LR_04_7.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <iomanip>
 #include <cmath>
 using namespace std;
 int main()
 {
-	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0, term = 0;
+	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
 	int n = 0;
-	const double PI = 3.1415926;
+	const double PI = 3.14159265359;
 	cout << "xp = "; cin >> xp;
 	cout << "xk = "; cin >> xk;
 	cout << "dx = "; cin >> dx;
 	cout << "eps = "; cin >> eps;
-	
+
 	cout << fixed;
 	cout << "-----------------------------------------" << endl;
 	cout << "|" << setw(7) << "x" << " |"
@@ -23,19 +23,19 @@ int main()
 	x = xp;
 	while (x <= xk)
 	{
-		n = 1; // вираз залежить від умови завдання варіанту
-		a = x; // вираз залежить від умови завдання варіанту
+		n = 1; 
+		a = (x * x * x)/6; 
 		S = a;
 		do {
 			n++;
-			R = ((2.0 * n - 1) * (2.0 * n - 1)) / (2.0 * n * (2.0 * n + 1)) * x * x;// вираз залежить від умови завдання варіанту
+			R = ((2.0 * n - 1) * (2.0 * n - 1)) / (2.0 * n * (2.0 * n + 1)) *( x * x);
 			a *= R;
 			S += a;
 
 		} while (abs(a) >= eps);
 		cout << "|" << setw(7) << setprecision(2) << x << " |"
-			<< setw(10) << setprecision(5) << acos(x) << " |"  // Точне значення arccos
-			<< setw(10) << setprecision(5) << ( PI / 2 - S) << " |"  // Наближене значення через ряд
+			<< setw(10) << setprecision(5) << acos(x) << " |"  
+			<< setw(10) << setprecision(5) << (PI / 2.0) - x - S << " |"  
 			<< setw(5) << n << " |"
 			<< endl;
 		x += dx;
